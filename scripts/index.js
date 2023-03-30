56408a129643a2f6303a17cc21baedd6f5f4d7b8
Gonzalo Sierra <gonzalo.sierra@arcelormittal.com.ar>
2023-03-30 01:25:00 -0300

    First version

---
+++ scripts/index.js
@@ -0,0 +1,138 @@
+$(document).ready(function () {
+
+    $('#btnAutoGestion').mouseenter(function () {
+        $('#collapseAutoGestion').collapse("show");
+    });
+ 
+    $('#btnAutoGestion').mouseleave(function () {
+        $('#collapseAutoGestion').collapse("hide");
+    });
+
+    $('#btnAutoGestion').mouseenter(function () {
+        $('#collapseAutoGestion').collapse("show");
+    });
+ 
+    $('#btnAutoGestion').mouseleave(function () {
+        $('#collapseAutoGestion').collapse("hide");
+    });
+
+    $('#btnIdm').mouseenter(function () {
+        $('#collapseIdm').collapse("show");
+    });
+ 
+    $('#btnIdm').mouseleave(function () {
+        $('#collapseIdm').collapse("hide");
+    });
+
+    $('#btnDesempenio').mouseenter(function () {
+        $('#collapseDesempenio').collapse("show");
+    });
+ 
+    $('#btnDesempenio').mouseleave(function () {
+        $('#collapseDesempenio').collapse("hide");
+    });
+    
+    $('#btnSis').mouseenter(function () {
+        $('#collapseSis').collapse("show");
+    });
+
+    $('#btnSis').mouseleave(function () {
+        $('#collapseSis').collapse("hide");
+    });
+
+    $('#btnSnow').mouseenter(function () {
+        $('#collapseSnow').collapse("show");
+    });
+
+    $('#btnSnow').mouseleave(function () {
+        $('#collapseSnow').collapse("hide");
+    });
+
+    $('#btnIdear').mouseenter(function () {
+        $('#collapseIdear').collapse("show");
+    });
+
+    $('#btnIdear').mouseleave(function () {
+        $('#collapseIdear').collapse("hide");
+    });
+
+    $('#btnGuardias').mouseenter(function () {
+        $('#collapseGuardias').collapse("show");
+    });
+
+    $('#btnGuardias').mouseleave(function () {
+        $('#collapseGuardias').collapse("hide");
+    });
+
+    $('#btnFiori').mouseenter(function () {
+        $('#collapseFiori').collapse("show");
+    });
+
+    $('#btnFiori').mouseleave(function () {
+        $('#collapseFiori').collapse("hide");
+    });
+
+    $('#btnChatbot').mouseenter(function () {
+        $('#collapseChatbot').collapse("show");
+    });
+
+    $('#btnChatbot').mouseleave(function () {
+        $('#collapseChatbot').collapse("hide");
+    });
+
+    $('#btnRecorrido').mouseenter(function () {
+        $('#collapseRecorrido').collapse("show");
+    });
+
+    $('#btnRecorrido').mouseleave(function () {
+        $('#collapseRecorrido').collapse("hide");
+    });
+
+    $('#btnRemis').mouseenter(function () {
+        $('#collapseRemis').collapse("show");
+    });
+
+    $('#btnRemis').mouseleave(function () {
+        $('#collapseRemis').collapse("hide");
+    });
+
+    $('#btnCompra').mouseenter(function () {
+        $('#collapseCompra').collapse("show");
+    });
+
+    $('#btnCompra').mouseleave(function () {
+        $('#collapseCompra').collapse("hide");
+    });
+
+    $('#btnOrganigrama').mouseenter(function () {
+        $('#collapseOrganigrama').collapse("show");
+    });
+
+    $('#btnOrganigrama').mouseleave(function () {
+        $('#collapseOrganigrama').collapse("hide");
+    });
+
+    $('#btnReserva').mouseenter(function () {
+        $('#collapseReserva').collapse("show");
+    });
+
+    $('#btnReserva').mouseleave(function () {
+        $('#collapseReserva').collapse("hide");
+    });
+
+    $('#btnProveedores').mouseenter(function () {
+        $('#collapseProveedores').collapse("show");
+    });
+
+    $('#btnProveedores').mouseleave(function () {
+        $('#collapseProveedores').collapse("hide");
+    });
+
+    $('#btnOffice').mouseenter(function () {
+        $('#collapseOffice').collapse("show");
+    });
+
+    $('#btnOffice').mouseleave(function () {
+        $('#collapseOffice').collapse("hide");
+    });
+});
